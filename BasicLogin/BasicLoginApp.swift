2e4dfa4dc2c92c78487dae1ca7bec2614a31bd82
Teo Toledo <teo@koibanx.com>
2024-06-06 23:59:07 -0300

    Initial Commit

---
+++ BasicLogin/BasicLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicLoginApp.swift
+//  BasicLogin
+//
+//  Created by Teo Toledo on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BasicLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
